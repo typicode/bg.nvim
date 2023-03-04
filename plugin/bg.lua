08505b2a5c4a6922ffe8b34e1e4f75c7613f4804
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-04 15:35:49 +0700

    add UIEnter event to update autocmd

--- plugin/bg.lua
+++ plugin/bg.lua
@@ -16,7 +16,7 @@ local update = function()
 end
 
 local setup = function()
-	vim.api.nvim_create_autocmd({ "ColorScheme" }, { callback = update })
+	vim.api.nvim_create_autocmd({ "ColorScheme", "UIEnter" }, { callback = update })
 	vim.api.nvim_create_autocmd({ "VimLeavePre" }, { callback = reset })
 end
 
